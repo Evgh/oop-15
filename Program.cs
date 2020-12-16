034ab7f3b835bace4fd314e78f40a7633de5272c
Evgh <kasperovich.official@gmail.com>
2020-12-16 21:56:59 +0300

    1st task: Get Processes

--- Program.cs
+++ Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Diagnostics;
 
 namespace oop_15
 {
@@ -10,6 +7,36 @@ namespace oop_15
     {
         static void Main(string[] args)
         {
+            foreach (Process process in Process.GetProcesses())
+            {
+
+                try
+                {
+                    Console.WriteLine($"ID: {process.Id}");
+                    Console.WriteLine($"Name: { process.ProcessName} ");
+                    Console.WriteLine($"Physical memory usage: {process.WorkingSet64}");
+                    Console.WriteLine($"Base priority: {process.BasePriority}");
+                    Console.WriteLine($"Virtual memory size: {process.VirtualMemorySize64}");
+                    Console.WriteLine($"Paged system memory size: {process.PagedSystemMemorySize64}");
+                    Console.WriteLine($"Paged memory size: {process.PagedMemorySize64}");
+                    Console.WriteLine($"Start Time: {process.StartTime}");
+                    Console.WriteLine($"Priority class: {process.PriorityClass}");
+                    Console.WriteLine($"User processor time: {process.UserProcessorTime}");
+                    Console.WriteLine($"Privileged processor time: {process.PrivilegedProcessorTime}");
+                    Console.WriteLine($"Total processor time: {process.TotalProcessorTime}");
+                    Console.WriteLine();
+                }
+                catch
+                {
+                    Console.WriteLine();
+                }
+
+                
+            }
+
+
+
+            Console.ReadKey();
         }
     }
 }
